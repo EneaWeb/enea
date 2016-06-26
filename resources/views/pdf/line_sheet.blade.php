951fbdbcf3e09cc7178fff753485e19dc9e4c0aa
eneaweb <giovannicweb@gmail.com>
2016-06-26 03:38:52 +0200

    line sheet generator

--- resources/views/pdf/line_sheet.blade.php
+++ resources/views/pdf/line_sheet.blade.php
@@ -38,29 +38,51 @@
 </head>
 <body style="width:100%">
 <div id="container" style="font-family:'Open Sans', sans-serif; width:100%;">
-
-    <h1>{!!strtoupper($brand->name)!!} LINE SHEET {!!\App\Season::find(\App\Option::where('name', 'active_season')->first()->value)->name!!}</h1>
-    <br><br><br>
-    <table class="bordered" style="width:100%; background-color:red">
+    <div style="display:inline-block; float:left; width:36%; text-align:left">
+        <img src="{{public_path()}}/assets/images/brands/{!!$brand->logo!!}" style="max-height:150px"/>
+    </div>
+    <div style="display:inline-block; float:left; width:64%; text-align:left">
+        <h1>
+        <br><br><br><br>
+            {!!strtoupper($brand->name)!!} LINE SHEET {!!\App\Season::find(\App\Option::where('name', 'active_season')->first()->value)->name!!}
+        </h1>
+    </div>
+    <br><br>
+    <table class="bordered" style="width:100%;">
         @foreach ($products as $product)
+        @if ($product->slug != '_custom')
+            <tr><td></td></tr>
             <tr>
                 <th style="text-align:right; width:310px">
                     <img style="max-width:300px" src="{!!public_path()!!}/assets/images/products/{!!$brand->slug!!}/{!!$product->picture!!}"/>
                 </th>
-                <th style="text-align:left"><h1>{!!$product->prodmodel->name!!} / {!!$product->name!!}</h1>
+                <th style="text-align:center; background-color:#ECECEC"><h1><u>{!!$product->prodmodel->name!!} / {!!$product->name!!}</u></h1>
                 <p>{!!$product->description!!}</p>
                 </th>
             </tr>
             @foreach ($product->variations as $variation)
+            
                 <tr>
                     <td style="text-align:right; width:310px">
-                        <img style="max-width:130px" src="{!!public_path()!!}/assets/images/products/{!!$brand->slug!!}/{!!$variation->picture!!}"/>
+                        <img style="max-width:140px" src="{!!public_path()!!}/assets/images/products/{!!$brand->slug!!}/{!!$variation->picture!!}"/>
                     </td>
-                    <td style="text-align:left">{!!\App\ItemPrice::where('season_list_id', 2)->where('item_id', \App\Item::where('product_id', $product->id)->first()['id'])->first()['price']!!}</td>
+                    <td style="text-align:left; background-color:#ECECEC;">
+                        <h3>
+                            {!!$product->prodmodel->name!!} / {!!$product->name!!}
+                        </h3>
+                        <h3>
+                            col. {!!\App\Color::find($variation->color_id)->name!!}
+                        </h3>
+                        <h3>
+                        
+                            International: € {!!\App\ItemPrice::where('season_list_id', 2)->where('item_id', \App\Item::where('product_id', $product->id)->first()['id'])->first()['price']!!}
+                        </h3>
+                    </td>
                 </tr>
+            
             @endforeach
-        
-        @endforeach       
+        @endif
+        @endforeach  
     
     </table>
     
