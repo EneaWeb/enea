13c0f7fc6e6b73560906b7ea372e3c57fd4c8cc4
eneaweb <giovannicweb@gmail.com>
2016-06-28 14:26:08 +0200

    .

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -64,7 +64,7 @@ class PDFController extends Controller
 		
 		// return view('pdf.line_sheet', compact('brand', 'products', 'seasonlist');
 		//return $pdf->stream();
-		return $pdf->download($brand->name.' Line Sheet '.\App\Season::find(\App\Option::where('name', 'active_season')->first()->value)->name.' '.$sesonlist->name.'.pdf');	
+		return $pdf->download($brand->name.' Line Sheet '.\App\Season::find(\App\Option::where('name', 'active_season')->first()->value)->name.' '.$seasonlist->name.'.pdf');	
 	}
 
 }
