0556b4e59146f655276c29493a8f661724715fae
eneaweb <giovannicweb@gmail.com>
2016-07-07 14:43:09 +0200

    test

--- resources/views/pages/customers/_modal_add_customer.blade.php
+++ resources/views/pages/customers/_modal_add_customer.blade.php
@@ -43,105 +43,109 @@
                         {!!Form::label('address', trans('menu.Address').'*', ['class' => 'col-md-3 control-label'])!!}
                     <div class="col-md-8">
                         
-                        <input id="pac-input" name="address" class="controls" type="text"
-                                placeholder="Enter a location">
-                            <div id="type-selector" class="controls">
-                              <input type="radio" name="type" id="changetype-all" checked="checked">
-                              <label for="changetype-all">All</label>
+                      <div id="locationField">
+                        <input id="autocomplete" placeholder="Enter your address"
+                               onFocus="geolocate()" type="text"></input>
+                      </div>
 
-                              <input type="radio" name="type" id="changetype-establishment">
-                              <label for="changetype-establishment">Establishments</label>
+                      <table id="address">
+                        <tr>
+                          <td class="label">Street address</td>
+                          <td class="slimField"><input class="field" id="street_number"
+                                disabled="true"></input></td>
+                          <td class="wideField" colspan="2"><input class="field" id="route"
+                                disabled="true"></input></td>
+                        </tr>
+                        <tr>
+                          <td class="label">City</td>
+                          <td class="wideField" colspan="3"><input class="field" id="locality"
+                                disabled="true"></input></td>
+                        </tr>
+                        <tr>
+                          <td class="label">State</td>
+                          <td class="slimField"><input class="field"
+                                id="administrative_area_level_1" disabled="true"></input></td>
+                          <td class="label">Zip code</td>
+                          <td class="wideField"><input class="field" id="postal_code"
+                                disabled="true"></input></td>
+                        </tr>
+                        <tr>
+                          <td class="label">Country</td>
+                          <td class="wideField" colspan="3"><input class="field"
+                                id="country" disabled="true"></input></td>
+                        </tr>
+                      </table>
 
-                              <input type="radio" name="type" id="changetype-address">
-                              <label for="changetype-address">Addresses</label>
+                      <script>
+                        // This example displays an address form, using the autocomplete feature
+                        // of the Google Places API to help users fill in the information.
 
-                              <input type="radio" name="type" id="changetype-geocode">
-                              <label for="changetype-geocode">Geocodes</label>
-                            </div>
-                            <div id="map"></div>
+                        // This example requires the Places library. Include the libraries=places
+                        // parameter when you first load the API. For example:
+                        // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
-                            <script>
-                              // This example requires the Places library. Include the libraries=places
-                              // parameter when you first load the API. For example:
-                              // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+                        var placeSearch, autocomplete;
+                        var componentForm = {
+                          street_number: 'short_name',
+                          route: 'long_name',
+                          locality: 'long_name',
+                          administrative_area_level_1: 'short_name',
+                          country: 'long_name',
+                          postal_code: 'short_name'
+                        };
 
-                              function initMap() {
-                                var map = new google.maps.Map(document.getElementById('map'), {
-                                  center: {lat: -33.8688, lng: 151.2195},
-                                  zoom: 13
-                                });
-                                var input = /** @type {!HTMLInputElement} */(
-                                    document.getElementById('pac-input'));
+                        function initAutocomplete() {
+                          // Create the autocomplete object, restricting the search to geographical
+                          // location types.
+                          autocomplete = new google.maps.places.Autocomplete(
+                              /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
+                              {types: ['geocode']});
 
-                                var types = document.getElementById('type-selector');
-                                map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-                                map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+                          // When the user selects an address from the dropdown, populate the address
+                          // fields in the form.
+                          autocomplete.addListener('place_changed', fillInAddress);
+                        }
 
-                                var autocomplete = new google.maps.places.Autocomplete(input);
-                                autocomplete.bindTo('bounds', map);
+                        function fillInAddress() {
+                          // Get the place details from the autocomplete object.
+                          var place = autocomplete.getPlace();
 
-                                var infowindow = new google.maps.InfoWindow();
-                                var marker = new google.maps.Marker({
-                                  map: map,
-                                  anchorPoint: new google.maps.Point(0, -29)
-                                });
+                          for (var component in componentForm) {
+                            document.getElementById(component).value = '';
+                            document.getElementById(component).disabled = false;
+                          }
 
-                                autocomplete.addListener('place_changed', function() {
-                                  infowindow.close();
-                                  marker.setVisible(false);
-                                  var place = autocomplete.getPlace();
-                                  if (!place.geometry) {
-                                    window.alert("Autocomplete's returned place contains no geometry");
-                                    return;
-                                  }
+                          // Get each component of the address from the place details
+                          // and fill the corresponding field on the form.
+                          for (var i = 0; i < place.address_components.length; i++) {
+                            var addressType = place.address_components[i].types[0];
+                            if (componentForm[addressType]) {
+                              var val = place.address_components[i][componentForm[addressType]];
+                              document.getElementById(addressType).value = val;
+                            }
+                          }
+                        }
 
-                                  // If the place has a geometry, then present it on a map.
-                                  if (place.geometry.viewport) {
-                                    map.fitBounds(place.geometry.viewport);
-                                  } else {
-                                    map.setCenter(place.geometry.location);
-                                    map.setZoom(17);  // Why 17? Because it looks good.
-                                  }
-                                  marker.setIcon(/** @type {google.maps.Icon} */({
-                                    url: place.icon,
-                                    size: new google.maps.Size(71, 71),
-                                    origin: new google.maps.Point(0, 0),
-                                    anchor: new google.maps.Point(17, 34),
-                                    scaledSize: new google.maps.Size(35, 35)
-                                  }));
-                                  marker.setPosition(place.geometry.location);
-                                  marker.setVisible(true);
-
-                                  var address = '';
-                                  if (place.address_components) {
-                                    address = [
-                                      (place.address_components[0] && place.address_components[0].short_name || ''),
-                                      (place.address_components[1] && place.address_components[1].short_name || ''),
-                                      (place.address_components[2] && place.address_components[2].short_name || '')
-                                    ].join(' ');
-                                  }
-
-                                  infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-                                  infowindow.open(map, marker);
-                                });
-
-                                // Sets a listener on a radio button to change the filter type on Places
-                                // Autocomplete.
-                                function setupClickListener(id, types) {
-                                  var radioButton = document.getElementById(id);
-                                  radioButton.addEventListener('click', function() {
-                                    autocomplete.setTypes(types);
-                                  });
-                                }
-
-                                setupClickListener('changetype-all', []);
-                                setupClickListener('changetype-address', ['address']);
-                                setupClickListener('changetype-establishment', ['establishment']);
-                                setupClickListener('changetype-geocode', ['geocode']);
-                              }
-                            </script>
-                            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBg0uwhTd3hEwC10wl4i0PV1cs6paJemH4&libraries=places&callback=initMap"
-                                async defer></script>
+                        // Bias the autocomplete object to the user's geographical location,
+                        // as supplied by the browser's 'navigator.geolocation' object.
+                        function geolocate() {
+                          if (navigator.geolocation) {
+                            navigator.geolocation.getCurrentPosition(function(position) {
+                              var geolocation = {
+                                lat: position.coords.latitude,
+                                lng: position.coords.longitude
+                              };
+                              var circle = new google.maps.Circle({
+                                center: geolocation,
+                                radius: position.coords.accuracy
+                              });
+                              autocomplete.setBounds(circle.getBounds());
+                            });
+                          }
+                        }
+                      </script>
+                      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBg0uwhTd3hEwC10wl4i0PV1cs6paJemH4&libraries=places&callback=initAutocomplete"
+                          async defer></script>
                         
                         {{-- {!! $autocompleteHelper->renderHtmlContainer($autocomplete) !!} --}}
                         {{-- Google Autocomplete Script --}}
