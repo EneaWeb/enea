e47239efe5b4535df8399f875e469d289a693002
eneaweb <giovannicweb@gmail.com>
2016-07-10 12:59:43 +0200

    fix layout

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -4,7 +4,7 @@
 
 	<div class="row">
 	
-	   <div class="col-md-4">
+	   <div class="col-md-6">
 	      {{-- START PROJECTS BLOCK --}}
 	       <div class="panel panel-default">
 	           <div class="panel-heading">
@@ -81,7 +81,7 @@
 	       {{-- END PROJECTS BLOCK --}}
 	   </div>
 	
-	   <div class="col-md-8">
+	   <div class="col-md-12">
 	       
 	       {{-- START SALES BLOCK --}}
 			<div class="panel panel-default">
