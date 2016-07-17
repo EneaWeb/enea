dc7ec4041cc029e7147e3d657b6338b1d0f6eb4e
eneaweb <giovannicweb@gmail.com>
2016-07-18 00:44:48 +0200

    layout

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -86,9 +86,6 @@
 											{!!number_format(\App\Order::sum('total'), 2, ',','.') !!} €
 											</strong></td>
 											<td>
-												<div class="progress progress-small progress-striped active">
-	                                    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">100%</div>
-	                                 </div>
 	                              </td>
 	                           </tr>
 	                       </tbody>
