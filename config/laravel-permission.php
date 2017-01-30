2b495d32f0368557a4fb8a049b7e6950300f97c0
EneaWeb <giovannicweb@gmail.com>
2017-01-30 12:12:27 +0100

    custom user and permissions config

--- config/laravel-permission.php
+++ config/laravel-permission.php
@@ -63,6 +63,7 @@ return [
         */
         'users' => 'users',
 
+
         /*
         |--------------------------------------------------------------------------
         | Roles Table
@@ -100,7 +101,7 @@ return [
         |
         */
 
-        'user_has_permissions' => 'user_has_permissions',
+        'user_has_permissions' => 'user_permission',
 
         /*
         |--------------------------------------------------------------------------
@@ -113,7 +114,7 @@ return [
         |
         */
 
-        'user_has_roles' => 'user_has_roles',
+        'user_has_roles' => 'user_role',
 
         /*
         |--------------------------------------------------------------------------
@@ -126,7 +127,7 @@ return [
         |
         */
 
-        'role_has_permissions' => 'role_has_permissions',
+        'role_has_permissions' => 'permission_role',
 
     ],
 
